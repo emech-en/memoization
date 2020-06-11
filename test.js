b7b16d7826d14897a31ba1065553cb8af8f1482d
emech <mh.niroomand91@gmail.com>
2020-06-11 17:19:30 +0430

    test: update tests to use sinonjs-fake-timers

--- test.js
+++ test.js
@@ -1,33 +1,38 @@
 const memoization = require("./memoizaton");
 const expect = require("chai").expect;
-
+const sinon = require("sinon");
 // hint: use https://sinonjs.org/releases/v6.1.5/fake-timers/ for faking timeouts
 
 describe("memoization", function () {
-  it("should memoize function result", (done) => {
+  let clock;
+  before(() => {
+    clock = sinon.useFakeTimers(Date.now());
+  });
+  after(() => {
+    clock.uninstall();
+  });
+
+  it("should memoize function result", () => {
     const testFunction = (year, month, day) => {
       return Date.now() + Date(year, month, day);
     };
 
-    const memoized = memoization.memoize(
+    const testMemoizedFunction = memoization.memoize(
       testFunction,
       (year, month, day) => year + month + day,
       1000
     );
-    const firstResult = memoized(1, 11, 26);
-    const secondResult = memoized(1, 11, 26);
+
+    const firstResult = testMemoizedFunction(1, 11, 26);
+    const secondResult = testMemoizedFunction(1, 11, 26);
     expect(firstResult).to.equal(secondResult);
 
-    setTimeout(() => {
-      const thirdResult = memoized(1, 11, 26);
-      expect(thirdResult).to.equal(secondResult);
-      setTimeout(() => {
-        const forthResult = memoized(1, 11, 26);
-        expect(forthResult).not.to.equal(secondResult);
-        done();
-      }, 1000);
-    }, 500);
-  });
+    clock.tick(500);
+    const thirdResult = testMemoizedFunction(1, 11, 26);
+    expect(thirdResult).to.equal(secondResult);
 
-  // TODO additional tests required
+    clock.tick(1000);
+    const forthResult = testMemoizedFunction(1, 11, 26);
+    expect(forthResult).not.to.equal(secondResult);
+  });
 });
